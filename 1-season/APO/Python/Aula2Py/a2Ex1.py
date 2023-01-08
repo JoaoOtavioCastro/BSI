e86b5d01db4c46b9c37b84ec011cbb91477e3341
João Otávio Oliveira Castro <jotaviocastro359@gmail.com>
2023-01-08 18:45:48 -0300

    clear

--- 1-season/APO/Python/Aula2Py/a2Ex1.py
+++
@@ -1,4 +0,0 @@
-x = int(input('Informe a quantidade de latas de 350ml foram compradas: '))
-y = int(input('Informe a quantidade de garrafas de 600ml foram compradas: '))
-z = int(input('Informe a quantidade de garrafas de 2L foram compradas: '))
-print(f" foram comprados {(x*0.35)+(y*0.6)+(z*2)} litros")
--- 1-season/APO/Python/PyAula1/Aula1Ex2.py
+++
@@ -1,3 +0,0 @@
-#receba o salario base do funcionario e calcule o salario a receber sabendo q paga imposto de 10% sobre a base e ganha gratificação de 50 reais
-x = float(input("Informe o Salário base do funcionario? "))
-print(f"O Salário a receber será de: R${(x*0.9)+50}")
